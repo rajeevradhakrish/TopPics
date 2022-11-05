b07ef8d842b9abfc1a8527d0f52cddbbdbada3ba
Rajeev Radhakrishnan <rajeev.radhakrishnan@theceomagazine.com>
2022-11-05 21:06:57 +0530

    Created a Configuration file to manage configuration items

---
+++ TopPics/Configuration.swift
@@ -0,0 +1,33 @@
+//
+//  Configuration.swift
+//  TopPics
+//
+//  Created by Rajeev Radhakrishnan on 05/11/22.
+//
+
+import Foundation
+
+struct server{
+    struct url
+    {
+     static let imgurUrl = ""
+    }
+    struct endPoints
+    {
+        static let search = ""
+    }
+    struct profile
+    {
+        static let clientID = "bf1b699f719d022"
+        static let clientSecret = "643212d5ab7950bc44b48ebca47c9da84296d2c4"
+    }
+    
+}
+
+struct theme
+{
+    struct colors
+    {
+        static let themeMain = ""
+    }
+}
---
+++ TopPics/GalleryVC.swift
@@ -0,0 +1,21 @@
+//
+//  GalleryVC.swift
+//  TopPics
+//
+//  Created by Rajeev Radhakrishnan on 05/11/22.
+//
+
+import UIKit
+
+class GalleryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
